a9784d51c48f5ca25ad09d96846f03f8afaeac89
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-03-13 12:24:40 +0000

    :construction: master - Add getters to Crop class :construction:

--- ada-farm-stupot83/models/Crop.js
+++ ada-farm-stupot83/models/Crop.js
@@ -1,34 +1,64 @@
 class Crop {
     constructor(type, field, x, y) {
         this.type = type;
-        this.image = loadImage('./images/' + type.toLowerCase() + '.png');
-        this.x = x;
-        this.y = y;
-        this.birthstamp = millis() / 1000;
-        this.width = 1;
-        this.height = 1;
-        this.healthySize = 30;
+        this._image = loadImage('./images/' + type.toLowerCase() + '.png');
+        this.health = 0;
+        this.field = field;
+        this._x = x;
+        this._y = y;
+        this._birthstamp = millis() / 1000;
+        this._width = 1;
+        this._height = 1;
+        this._healthySize = 30;
         this.draw();
     }
 
+    getBirthStamp() {
+        return this._birthstamp;
+    }
+
     getAge() {
-        return millis() / 1000 - this.birthstamp;
+        return millis() / 1000 - this.getBirthStamp();
+    }
+
+    getHealthySize() {
+        return this._healthySize;
+    }
+
+    getWidth() {
+        return this._width;
+    }
+
+    getHeight() {
+        return this._height;
+    }
+
+    getImage() {
+        return this._image;
+    }
+
+    getXPosition() {
+        return this._x;
+    }
+
+    getYPosition() {
+        return this._y;
     }
 
     draw() {
 
-        if (this.getAge() < this.healthySize) {
-            var width = this.width * this.getAge();
-            var height = this.height * this.getAge();
+        if (this.getAge() < this.getHealthySize()) {
+            var width = this.getWidth() * this.getAge();
+            var height = this.getHeight() * this.getAge();
         } else {
             var width = 30;
             var height = 30;
         }
 
         image(
-            this.image,
-            this.x,
-            this.y,
+            this.getImage(),
+            this.getXPosition(),
+            this.getYPosition(),
             width,
             height
         );
