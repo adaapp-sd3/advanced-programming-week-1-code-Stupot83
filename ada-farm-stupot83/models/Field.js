29e16efb106013ad03530838fc644f4a33a7f971
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-03-13 12:33:36 +0000

    :construction: Add getters to Field class :construction:

--- ada-farm-stupot83/models/Field.js
+++ ada-farm-stupot83/models/Field.js
@@ -1,20 +1,44 @@
 class Field {
     constructor(name, health, x, y, width, height) {
-        this.name = name;
+        this._name = name;
         this.health = health;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.crops = [];
+        this._x = x;
+        this._y = y;
+        this._width = width;
+        this._height = height;
+        this._crops = [];
+    }
+
+    getName() {
+        return this._name;
+    }
+
+    getXPosition() {
+        return this._x;
+    }
+
+    getYPosition() {
+        return this._y;
+    }
+
+    getWidth() {
+        return this._width;
+    }
+
+    getHeight() {
+        return this._height;
+    }
+
+    getCrops() {
+        return this._crops;
     }
 
     draw() {
         stroke('#7c4011');
         strokeWeight(10);
         fill('#b58969');
-        rect(this.x, this.y, this.width, this.height, 10);
-        for (var crop of this.crops) {
+        rect(this.getXPosition(), this.getYPosition(), this.getWidth(), this.getHeight(), 10);
+        for (var crop of this.getCrops()) {
             crop.draw();
         }
     }
@@ -22,16 +46,16 @@ class Field {
     plant(x, y) {
         console.log('new plant');
         // Get the crop name from the field name
-        var cropName = this.name.split(' '); // => e.g. "Wheat"
+        var cropName = this.getName().split(' '); // => e.g. "Wheat"
         // Add a new crop to the field's list of crops
-        this.crops.push(new Crop(cropName[0], this, x, y));
+        this.getCrops().push(new Crop(cropName[0], this, x, y));
     }
 
     containsPoint(x, y) {
-        var top = this.y;
-        var bottom = this.y + this.height;
-        var left = this.x;
-        var right = this.x + this.width;
+        var top = this.getYPosition();
+        var bottom = this.getYPosition() + this.getHeight();
+        var left = this.getXPosition();
+        var right = this.getXPosition() + this.getWidth();
 
         var isInVerticalRange = y > top && y < bottom;
         var isInHorizontalRange = x > left && x < right;
